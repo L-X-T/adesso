e84c254fab9855137b4e6c23a7d1c37c22aca1f8
Alex Thalhammer <alex@angular-software.at>
2021-10-05 20:19:38 +0200

    [U] Add universal SSR

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     FlightBookingModule,
 
@@ -26,7 +26,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
     FlightLibModule.forRoot(),
     SharedModule.forRoot(),
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES, {
+    initialNavigation: 'enabled'
+})
   ],
   declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, BasketComponent],
   providers: [],
