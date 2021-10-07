f1cad12c676d3c46ed5c820198bdfd9ba974a270
Alex Thalhammer <alex@angular-software.at>
2021-10-07 12:20:26 +0200

    [Td-F] Parameterizable validator

--- apps/flight-app/src/app/shared/validation/city-validator.directive.ts
+++ apps/flight-app/src/app/shared/validation/city-validator.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
@@ -12,8 +12,11 @@ import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@an
   ]
 })
 export class CityValidatorDirective implements Validator {
+  @Input() city: string[] = [];
+
   validate(control: AbstractControl): ValidationErrors | null {
-    const validCities: string[] = ['Hamburg', 'Graz', 'Berlin', 'Wien'];
+    const validCities = this.city.length ? this.city : ['Hamburg', 'Graz', 'Berlin', 'Wien'];
+
     if (control.value && validCities.indexOf(control.value) === -1) {
       return {
         city: {
