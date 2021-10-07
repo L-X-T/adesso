df637ec9a419a4078e2da12d9ca10c6d8c72b0d1
Alex Thalhammer <alex@angular-software.at>
2021-10-07 13:18:29 +0200

    [Td-F] Bonus: Asynchronous Multifield Validator

--- apps/flight-app/src/app/shared/shared.module.ts
+++ apps/flight-app/src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { CityPipe } from './pipes/city.pipe';
 import { CityValidatorDirective } from './validation/city-validator.directive';
 import { AsyncCityValidatorDirective } from './validation/async-city-validator.directive';
 import { RoundTripValidatorDirective } from './validation/round-trip-validator.directive';
+import { AsyncFlightValidatorDirective } from './validation/async-flight-validator.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CityPipe, CityValidatorDirective, AsyncCityValidatorDirective, RoundTripValidatorDirective],
-  exports: [CityPipe, CityValidatorDirective, AsyncCityValidatorDirective, RoundTripValidatorDirective]
+  declarations: [CityPipe, CityValidatorDirective, AsyncCityValidatorDirective, RoundTripValidatorDirective, AsyncFlightValidatorDirective],
+  exports: [CityPipe, CityValidatorDirective, AsyncCityValidatorDirective, RoundTripValidatorDirective, AsyncFlightValidatorDirective]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders<SharedModule> {
---
+++ apps/flight-app/src/app/shared/validation/async-flight-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AsyncFlightValidatorDirective } from './async-flight-validator.directive';
+
+describe('AsyncMultiFieldValidatorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AsyncFlightValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
